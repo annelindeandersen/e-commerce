15bf24f23c16fc7f91d841a5a92db90bc6ff748e
Anne Linde <anne_linde@yahoo.dk>
2021-05-03 23:08:47 +0200

    Checkout and payment method added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
@@ -6,9 +6,29 @@ import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
 
-const Navbar = ({ totalItems }) => {
+const PrimarySearchAppBar = ({ totalItems }) => {
   const classes = useStyles();
   const location = useLocation();
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  const renderMobileMenu = (
+    <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
+      <MenuItem>
+        <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
+          <Badge badgeContent={totalItems} color='secondary'>
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <>
@@ -16,7 +36,7 @@ const Navbar = ({ totalItems }) => {
         <Toolbar>
           <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
             <img src={logo} alt='Commerce.js' height='25px' className='classes.image' />
-            Commerce.js
+            <div style={{ marginLeft: '10px' }}>The Tech Shop</div>
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
@@ -30,8 +50,9 @@ const Navbar = ({ totalItems }) => {
           )}
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </>
   );
 };
 
-export default Navbar;
+export default PrimarySearchAppBar;
